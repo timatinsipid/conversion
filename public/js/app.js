c8e11313f4f0223082f0a9d13951f53e610ad8c8
timatinsipid <tim@insipid.org.uk>
2022-10-07 12:57:14 -0700

    remove conversion button for cad

--- public/js/app.js
+++ public/js/app.js
@@ -4,34 +4,39 @@ const cadPrice = document.querySelector('#cadPrice')
 const usPrice = document.querySelector('#usPrice')
 const cadm1 = document.querySelector('#cadmessage1')
 const cadm2 = document.querySelector('#cadmessage2')
-const usm1 = document.querySelector('#usmessage1')
-const usm2 = document.querySelector('#usmessage2')
+const currentRate = document.querySelector('#currentRate')
+const currentRateForm = document.querySelector('#currentRateForm')
+const usmessage = document.querySelector('#usmessage')
 
-
-usdForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const amount = usPrice.value
-    const url = "/quote?amount=" + amount
-    usm1.textContent = "loading"
-    usm2.textContent = ""
+currentRateForm.addEventListener('submit', (rateEvent) => {
+    rateEvent.preventDefault();
+    const url = "/quotetest?amount=" + usPrice.value
+    currentRate.textContent = "Updating"
     fetch(url)
-    .then(response => response.json())
-    .then(data => {
+        .then(response => response.json())
+        .then(data => {
             if (data.error) { m1.textContent = data.error }
             else {
                 const rate = parseFloat(data.quotes.USDCAD)
-                usm1.textContent = "Live Rate " + rate 
-                usm2.textContent = "CAD $"+(parseFloat(usPrice.value)*rate).toFixed(2)
+                currentRate.textContent = rate
             }
         })
 })
-    
 
-cadPrice.addEventListener('blur', (e) => {
-    e.preventDefault();
+usdForm.addEventListener('submit', (usEvent) => {
+    usEvent.preventDefault();
+    const amount = parseFloat(usPrice.value)
+    const rate = currentRate.textContent
+    const finalValue = (amount * rate).toFixed(2)
+    usmessage.textContent = "CAD $" + finalValue
+})
+
+
+cadPrice.addEventListener('blur', (cadEvent) => {
+    cadEvent.preventDefault();
     const amount = parseFloat(cadPrice.value)
-    const tax = amount*0.12
-    const total = parseFloat(amount)+parseFloat(tax)
-    cadm1.textContent = "Tax amount $" + tax.toFixed(2) 
+    const tax = amount * 0.12
+    const total = parseFloat(amount) + parseFloat(tax)
+    cadm1.textContent = "Tax amount $" + tax.toFixed(2)
     cadm2.textContent = "Total after tax $" + total.toFixed(2)
 })
